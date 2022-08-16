18e3a29dc3d75fa9c0c1ca645429bed0b0c8db4e
Kim BeomJung <kbjoong@daum.net>
2022-08-16 16:45:30 +0900

    Update ch9_01.sql

--- MySQL/ch9_01.sql
+++ MySQL/ch9_01.sql
@@ -39,3 +39,28 @@ select @myVar1 + @myVar2; -- 변수에 임시 저장됨.
 set @txt = '가수 이름 ==> ';
 set @height = 166;
 select @txt, mem_name from member where height > @height;
+
+set @count = 3;
+select mem_name, height from member order by height limit @count; -- 오류. 문법적으로 지원하지 않음.
+
+set @count = 3;
+prepare mySQL from 'select mem_name, height from member order by height limit ?';
+execute mySQL using @count;
+
+select avg(price) '평균 가격' from buy; -- 결과 : 142.9167
+-- 명시적 변환
+select cast(avg(price) as signed) '평균 가격' from buy; -- 부호가 있는 정수로 나타냄. 결과 : 143
+
+select cast('2022$12$12' as date); -- 2022-12-12 출력
+select cast('2022/12/12' as date); -- 동일한 결과
+select cast('2022%12%12' as date); -- 동일한 결과
+select cast('2022@12@12' as date); -- 동일한 결과
+
+select num, concat(cast(price as char), 'X', cast(amount as char), '=') '가격X수량', price*amount '구매액' from buy;
+-- 암시적 형변환
+select '100' + '200'; -- 문자와 문자를 더함(정수로 변환되서 연산됨)
+select concat('100', '200'); -- 문자와 문자를 연결(문자로 처리)
+select concat(100, '200'); -- 정수와 문자를 연결(정수가 문자로 변환되서 처리)
+select 1 > '2mega'; -- 정수 2로 변환되어서 비교
+select 3 > '2mega'; -- 정수 2로 변환되어서 비교
+select 0 = 'mega2'; -- 문자는 0으로 변환됨
